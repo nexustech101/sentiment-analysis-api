ab563ab303754d252eb59f8ed0ecc038441f1f77
nexustech101 <charlesldefreeseiii@gmail.com>
2025-02-23 21:59:42 -0500

    Fixed a bug from the previous integration

--- models/models.py
+++ models/models.py
@@ -3,71 +3,73 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
+# Base model to be inherited by al other sentiment models
+class Base(BaseModel):
+    __info__: str = "Inherits from SentimentRequest parent (Base) model"
+
+
 # Full sentiment analysis models (abstracts away models for comprehensive documentation and api usage)
-class FullSentimentRequest(BaseModel):
+class FullSentimentRequest(Base):
     prompts: List[str]
-    __info__: str = None
 
 
-class FullSentimentResult(BaseModel):
+class FullSentimentResult(Base):
     label: str
     score: float
-    __info__: str = None
 
 
-class FullSentimentResponse(BaseModel):
+class FullSentimentResponse(Base):
     sequence: str
     sentiments: List[SentimentResult]
-    __info__: str = None
 
 
 # Emotion sentiment models (abstracts away models for comprehensive documentation and api usage)
-class EmotionSentimentRequest(SentimentRequest):
-    __info__: str = "Inherits from SentimentRequest parent model"
+class EmotionSentimentRequest(FullSentimentRequest):
+    pass
 
 
-class EmotionSentimentResult(SentimentResult):
-    __info__: str = "Inherits from SentimentResult parent model"
+class EmotionSentimentResult(FullSentimentResult):
+    pass
 
 
-class EmotionSentimentResponse(SentimentResponse):
-    __info__: str = "Inherits from SentimentResponse parent model"
+class EmotionSentimentResponse(FullSentimentResponse):
+    pass
 
 
 # Tonality sentiment models (abstracts away models for comprehensive documentation and api usage)
-class ToneSentimentRequest(SentimentRequest):
-    __info__: str = "Inherits from SentimentRequest parent model"
+class ToneSentimentRequest(FullSentimentRequest):
+    pass
 
 
-class ToneSentimentResult(SentimentResult):
-    __info__: str = "Inherits from SentimentResult parent model"
+class ToneSentimentResult(FullSentimentResult):
+    pass
 
 
-class ToneSentimentResponse(SentimentResponse):
-    __info__: str = "Inherits from SentimentResponse parent model"
-
-
-# Speach sentiment models (abstracts away models for comprehensive documentation and api usage)
-class SpeachSentimentRequest(SentimentRequest):
-    __info__: str = "Inherits from SentimentRequest parent model"
-
-
-class SpeachSentimentResult(SentimentResult):
-    __info__: str = "Inherits from SentimentResult parent model"
-
-
-class SpeachSentimentResponse(SentimentResponse):
-    __info__: str = "Inherits from SentimentResponse parent model"
+class ToneSentimentResponse(FullSentimentResponse):
+    pass
 
 
 # Speach sentiment models (abstracts away models for comprehensive documentation and api usage)
-class SpeachSentimentRequest(SentimentRequest):
-    __info__: str = "Inherits from SentimentRequest parent model"
+class SpeachSentimentRequest(FullSentimentRequest):
+    pass
 
 
-class SpeachSentimentResult(SentimentResult):
-    __info__: str = "Inherits from SentimentResult parent model"
+class SpeachSentimentResult(FullSentimentResult):
+    pass
 
 
-class SpeachSentimentResponse(SentimentResponse):
-    __info__: str = "Inherits from SentimentResponse parent model"
+class SpeachSentimentResponse(FullSentimentResponse):
+    pass
+
+
+# Speach sentiment models (abstracts away models for comprehensive documentation and api usage)
+class SpeachSentimentRequest(FullSentimentRequest):
+    pass
+
+
+class SpeachSentimentResult(FullSentimentResult):
+    pass
+
+
+class SpeachSentimentResponse(FullSentimentResponse):
+    pass
