43c9dc0380625456a26b5b6e4809a38176941037
nexustech101 <charlesldefreeseiii@gmail.com>
2025-02-23 19:28:29 -0500

    Integrated new project structure for more comprehensive api intagration

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 import uvicorn
 from routers.routes import router as sentiment_router
+from routers.routes import router as test_router
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 app.include_router(sentiment_router)
+app.include_router(test_router)
--- routers/routes.py
+++ routers/routes.py
@@ -1,10 +1,8 @@
 # ./routers/routes.py
 
-from fastapi import FastAPI, HTTPException, APIRouter
-from pydantic import BaseModel
-from transformers import pipeline
+from fastapi import FastAPI, HTTPException, APIRouter, Response
 from typing import List
-from models.models import SentimentRequest, SentimentResponse, SentimentResult
+from models.models import SentimentRequest, SentimentResponse
 from models.sentiment import get_sentiment_analysis
 
 router = APIRouter(
@@ -14,17 +12,7 @@ router = APIRouter(
 @router.post("/sentiment", response_model=List[SentimentResponse])
 async def analyze_sentiment(request: SentimentRequest):
     try:
-        return get_sentiment_analysis(request.prompts)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
-
-
-@router.get("/test")
-async def test_route():
-    try:
-        return { "response": "API is working!" }
+        return Response(get_sentiment_analysis(request.prompts))
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
