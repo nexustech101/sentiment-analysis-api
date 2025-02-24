c5fe09cae51ce25280696310ce640f5d385896fd
nexustech101 <charlesldefreeseiii@gmail.com>
2025-02-24 18:16:16 -0500

    Made UX changes to api route logic

--- routers/sentiment_route.py
+++ routers/sentiment_route.py
@@ -14,28 +14,10 @@ router = APIRouter(
 
 # Route gets a sentiment class from json config and returns sentiments data to user
 @router.post("/custom_sentiment/{label_group}", response_model=List[SentimentResponse])
-def analyze_sentiment(label_group: str, request: SentimentRequest):
-    sentiment_labels = load_sentiment_labels()
-    if label_group is None:
-        candidate_labels = sentiment_labels.get("all")
-    else:
-        candidate_labels = sentiment_labels.get(label_group)
-    
-    if not candidate_labels:
-        raise HTTPException(status_code=400, detail="Invalid label group. Further sentiment customization is underway.")
-    try:
-        return get_sentiments(candidate_labels, request.prompts)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
-    
-    
-# Handle case where user does not provide label group
 @router.post("/custom_sentiment", response_model=List[SentimentResponse])
-def analyze_sentiment(request: SentimentRequest):
+def analyze_sentiment(request: SentimentRequest, label_group: str = None):
     sentiment_labels = load_sentiment_labels()
-    candidate_labels = sentiment_labels.get("all")
+    candidate_labels = sentiment_labels.get(label_group) if label_group else sentiment_labels.get("all")
     
     if not candidate_labels:
         raise HTTPException(status_code=400, detail="Invalid label group. Further sentiment customization is underway.")
